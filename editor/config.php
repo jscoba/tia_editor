e6a4cf325fdcd75cec7cb85edc713d9c7113696d
Javier Sáez de la Coba <jscoba@gmail.com>
2013-09-02 00:09:29 +0200

    Version beta

---
+++ editor/config.php
@@ -0,0 +1,9 @@
+<?php
+
+  define('MYSQL_SERVER',          'localhost');
+  define('MYSQL_DATABASE',        'profundiza');
+  define('MYSQL_USERNAME',        'prufundiza');
+  define('MYSQL_PASSWORD',        'profundiza');
+  define('UPLOAD_PATH',           '/var/www/tia-alu-public/upload/');
+
+?>
