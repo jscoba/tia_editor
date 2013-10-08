0a6a35e07488f4cb1b96468784c8c666a75b5f33
Javier Sáez de la Coba <jscoba@gmail.com>
2013-10-08 23:12:45 +0200

    Scracth de funcionalidad para borrar imagenes ya creadas.

---
+++ borrar.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<html>
+<head>
+	<meta name="author" content="Javier Sáez de la Coba" >
+	<title></title>
+
+</head>
+<body >
+<table style="border: 1pt solid black; height: 10%; width: 100%;"   >
+	<tr>
+		<td>Miniatura</td>
+		<td>Fecha inicio</td>
+		<td>Fecha fin</td>
+		<td>Nombre</td>
+		<td>Eliminar</td>	
+	</tr>
+</table>
+</body>
+</html>
